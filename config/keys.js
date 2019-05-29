2b3ee5b763e50f96206ddc6eda7edd3ffddd4833
LadJignesh <111jigu@gmail.com>
2019-05-29 22:05:02 +0100

    Tweaks

--- config/keys.js
+++ config/keys.js
@@ -2,6 +2,7 @@
 
 module.exports = {
     mongodb:{
-        dbURI: "mongodb://jerry:test123@ds239055.mlab.com:39055/jl-oauth-test"
+      //  dbURI: "mongodb://jerry:test123@ds239055.mlab.com:39055/jl-oauth-test",
+        dbURI: "mongodb+srv://fergal:fergal@cluster0-55tso.mongodb.net/test?retryWrites=true"
     }
 }
