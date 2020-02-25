da9865fae3ca3a7c06b649c7de3feb4e55a5f46c
LadJignesh <111jigu@gmail.com>
2020-02-25 23:14:10 +0000

    app changes

--- models/crazyCombinationModel.js
+++ models/crazyCombinationModel.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const crazyCombinationSchema = new Schema({
+const crazyCombinationEsbSchema = new Schema({
     name:String,
+    player:String,
+    school:String,
     everyDayObject:String,
     techObject:String,
     solution: String
 });
 
-const crazyCombination = mongoose.model('crazyCombinationSolution', crazyCombinationSchema, 'crazyCombinationSolutions');
+const crazyCombination = mongoose.model('crazyCombinationEsbSolution', crazyCombinationEsbSchema, 'crazyCombinationEsbSolutions');
 
 module.exports = crazyCombination;
--- routes/crazyCombinationRoutes.js
+++ routes/crazyCombinationRoutes.js
@@ -9,7 +9,7 @@ router.get('/',(req,res)=>{
     MongoClient.connect(keys.mongodb.dbURI,{useNewUrlParser:true}, function(err, db) {
         if (err) throw err;
         var dbo = db.db("jl-oauth-test");
-        dbo.collection("crazyCombinationSolutions").find({}).toArray(function(err, result) {
+        dbo.collection("crazyCombinationEsbSolutions").find({}).toArray(function(err, result) {
           if (err) res.status(400).json("Error Connecting DB");
           res.status(200).render('pages/cc-index.ejs',{length:result.length});
           db.close();
@@ -23,7 +23,7 @@ router.get('/view',(req,res)=>{
     MongoClient.connect(keys.mongodb.dbURI,{useNewUrlParser:true}, function(err, db) {
         if (err) throw err;
         var dbo = db.db("jl-oauth-test");
-        dbo.collection("crazyCombinationSolutions").find({}).toArray(function(err, result) {
+        dbo.collection("crazyCombinationEsbSolutions").find({}).toArray(function(err, result) {
           if (err) res.status(400).json("Error Connecting DB");
           //console.log(result);
           res.status(200).render('pages/cc-view.ejs',{data:result,length:result.length});
@@ -35,7 +35,9 @@ router.get('/view',(req,res)=>{
 
 router.post('/',(req,res)=>{
     const idea = new crazyCombination({
-        name:req.body.teamName,
+        name:req.body.brandName,
+        player:req.body.teamName,
+        school:req.body.school,
         everyDayObject:req.body.everyDayObject,
         techObject:req.body.techObject,
         solution: req.body.solution
@@ -43,7 +45,7 @@ router.post('/',(req,res)=>{
     MongoClient.connect(keys.mongodb.dbURI,{useNewUrlParser:true}, function(err, db) {
         if (err) throw err;
         var dbo = db.db("jl-oauth-test");
-        dbo.collection("crazyCombinationSolutions").insertOne(idea,function(err, result) {
+        dbo.collection("crazyCombinationEsbSolutions").insertOne(idea,function(err, result) {
           if (err) res.status(400).json("Error Connecting DB");
           res.status(201).redirect('/crazyCombinations');
           db.close();
@@ -55,7 +57,7 @@ router.post('/delete', (req,res)=>{
     MongoClient.connect(keys.mongodb.dbURI,{useNewUrlParser:true},function(err, db) {
         if (err) throw err;
         var dbo = db.db("jl-oauth-test");
-    dbo.collection('crazyCombinationSolutions', {}, function(err, solution) {
+    dbo.collection('crazyCombinationEsbSolutions', {}, function(err, solution) {
         solution.deleteMany({}, function(err, result) {
             if (err) {
                 console.log(err);
@@ -72,7 +74,7 @@ router.post('/delete/id', (req,res)=>{
         if (err) throw err;
         var dbo = db.db("jl-oauth-test");
         var myquery = { _id: new mongodb.ObjectID(req.body.id) };
-        dbo.collection("crazyCombinationSolutions").deleteOne(myquery, function(err, obj) {
+        dbo.collection("crazyCombinationEsbSolutions").deleteOne(myquery, function(err, obj) {
             if (err) throw err;
             console.log("1 document deleted");
             res.status(200).redirect('/crazyCombinations/view');
